0137f1bdfb0df9a8d4c10226179207992e274eb6
gilsunshine <gilsunshine@gmail.com>
2018-04-25 21:18:09 -0400

    heroku

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -30,7 +30,7 @@ export default canvas => {
     camera.updateProjectionMatrix()
   })
 
-  let HOST = location.origin.replace('https://rocky-stream-93260.herokuapp.com/')
+  let HOST = location.origin.replace(/^http/, 'ws')
   const connection = new WebSocket(HOST)
 
   connection.addEventListener('open', () => {
