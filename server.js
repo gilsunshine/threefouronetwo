921e511604829850924d6018ab4cada6db8518e9
gilsunshine <gilsunshine@gmail.com>
2018-04-27 11:47:56 -0400

    sockets

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const uuidv4 = require('uuid/v4');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000
-// const INDEX = path.join(__dirname, './public/index.html');
+const INDEX = path.join(__dirname, './public/index.html');
 
-app.use(express.static(__dirname + "public"))
-
+app.use(express.static(INDEX))
 var server = http.createServer(app)
 server.listen(PORT)
 
