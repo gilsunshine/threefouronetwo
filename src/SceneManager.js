f266b057c2e82b2b7c5ba184cd322e33effc9b9d
gilsunshine <gilsunshine@gmail.com>
2018-04-27 11:01:47 -0400

    sockets

--- src/SceneManager.js
+++ src/SceneManager.js
@@ -33,8 +33,7 @@ export default canvas => {
    // var HOST = location.origin.replace(/^http/, 'ws')
   // const connection = new WebSocket('ws://localhost:5000')
 
-  // var host = window.location.origin.replace(/^http/, 'ws')
-  var host = 'ws://rocky-stream-93260.herokuapp.com/'
+  var host = window.location.origin.replace(/^http/, 'ws')
   var connection = new WebSocket(host);
 
   connection.addEventListener('open', () => {
