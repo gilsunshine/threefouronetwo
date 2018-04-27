a482bf716a7e6d33a5c1c6fa0e4b2c4783c02357
gilsunshine <gilsunshine@gmail.com>
2018-04-27 15:27:14 -0400

    sockets

--- src/Instructions.js
+++ src/Instructions.js
@@ -17,7 +17,7 @@ class Instructions extends React.Component{
     return(
       <div onClick={this.hideInstructions}>
         {this.state.show ? <div style={divStyle}> <strong style={textStyle}>Welcome to ThreeFourOneTwo</strong>
-        <p style={textStyle}>See that blue dot? That's you. Any red dots you might see are other users.</p>
+        <p style={textStyle}>See that blue dot? That's you. Any red dots you might see are other users. When the yellow screen passes a dot it plays the note associated with that dot.</p>
         <p style={textStyle}>Press W, A, S, and D keys to move your dot in the x and y axes. Press Z and X to move up and down in the z axis.</p>
         <p style={textStyle}>The x axis controls the pitch, the y axis controls the tone and sustain, and the z axis controls where in the sequence your note is played.</p>
         <p style={textStyle}>Click and drag to rotate your view. Click on the instructions to hide them.</p> </div>  : <div style={instructionDivStyle}><strong style={instructionTextStyle}>Instructions</strong></div>}
