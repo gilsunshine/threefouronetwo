6fdc69e67ac2d3aed86d9b73a954fb27b7666406
gilsunshine <gilsunshine@gmail.com>
2018-05-10 11:16:48 -0400

    sockets

--- server.js
+++ server.js
@@ -16,25 +16,37 @@ let wss = new WebSocketServer({server: server})
 
 let allConnections = []
 
-wss.on('connection', (ws, req) => {
-  const ip = req.connection.remoteAddress
-  console.log(`${ip} just made a WS connection`);
-  allConnections.push(ws)
+setupSocket = (ws, req) => {
+  wss.on('connection', (ws, req) => {
+    const ip = req.connection.remoteAddress
+    console.log(`${ip} just made a WS connection`);
+    allConnections.push(ws)
 
-  ws.on('close', function close(){
-    let index = allConnections.indexOf(ws)
-    if (index > -1) {
-      allConnections.splice(index, 1);
-    }
-  })
-
-  ws.on('message', (payload) => {
-    allConnections.forEach(client => {
-      if (client !== ws){
-        client.send(payload)
+    ws.on('close', function close(e){
+      if(e.code === 1000){
+        let index = allConnections.indexOf(ws)
+        if (index > -1) {
+          allConnections.splice(index, 1);
+        }
+      }
+      else {
+        setTimeout(function(){setupSocket(ws, req)}, 1000)
       }
     })
+
+    ws.on('message', (payload) => {
+      allConnections.forEach(client => {
+        if (client !== ws){
+          client.send(payload)
+        }
+      })
+    })
   })
-})
+}
+
+setupSocket()
+// wss.on('connection', (ws, req) => {
+//   setupSocket(ws, req)
+// })
 
 console.log("LISTENING FOR WS CONNECTIONS ON PORT: ", PORT);
