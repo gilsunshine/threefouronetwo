5cb28ac83d1c6c160f7ce0dd19e018ad82758bc9
Gil Sunshine <32219998+gilsunshine@users.noreply.github.com>
2023-06-27 08:18:34 -0700

    port

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const uuidv4 = require('uuid/v4');
 const app = express();
 const path = require('path');
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 10000
 const BUILD = path.join(__dirname, './build');
 
 app.use(express.static(BUILD))
